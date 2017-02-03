d2a8582473d1095acc5768965c3a221149124d66
LeonidH <leanid.hubar@gmail.com>
2017-02-04 00:42:40 +0300

    frontend-nanodegree-mobile-portfolio/feature: implement caching files

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -2,32 +2,37 @@
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
 
 function logCRP() {
-  var t = window.performance.timing,
-    dcl = t.domContentLoadedEventStart - t.domLoading,
-    complete = t.domComplete - t.domLoading;
-  var stats = document.getElementById("crp-stats");
-  stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
+    const t = window.performance.timing;
+    const dcl = t.domContentLoadedEventStart - t.domLoading;
+    const complete = t.domComplete - t.domLoading;
+    const stats = document.getElementById("crp-stats");
+    stats.textContent = `DCL: ${dcl}ms, onload: ${complete}ms`;
 }
 
 function loadImages() {
-    var images = document.getElementsByTagName("img");
-    for (var n = 0; n < images.length; n++) {
-        images[n].src = images[n].getAttribute('data-src');
+    const images = document.getElementsByClassName("loadable");
+    for (let n = 0; n < images.length; n++) {
+        images[n].src = images[n].getAttribute("data-src");
     }
 }
 
-window.addEventListener("load", function(event) {
-    logCRP();
-    loadImages();
-
-    (function (w, g) {
-        w['GoogleAnalyticsObject'] = g;
-        w[g] = w[g] || function () { (w[g].q = w[g].q || []).push(arguments) };
-        w[g].l = 1 * new Date();
-    })(window, 'ga');
-    // Optional TODO: replace with your Google Analytics profile ID.
-    ga('create', 'UA-90954736-1', 'auto');
-    ga('send', 'pageview');
-});
-
-
+window.addEventListener("load",
+    function(event) {
+        logCRP();
+        loadImages();
+        (function(i, s, o, g, r, a, m) {
+            i["GoogleAnalyticsObject"] = r;
+            i[r] = i[r] ||
+                function() {
+                    (i[r].q = i[r].q || []).push(arguments);
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m);
+        })(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
+        // Optional TODO: replace with your Google Analytics profile ID.
+        ga("create", "UA-90954736-1", "auto");
+        ga("send", "pageview");
+    });
